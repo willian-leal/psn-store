dad008aaaba89cbe9f83ec0527752a50607a6f42
willian-leal <willian.oliveira@bandtec.com.br>
2023-10-05 17:37:56 -0300

    create component card

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+	@Input()
+	gameCover: string = ""
+	@Input()
+	gameLabel: string = ""
+	@Input()
+	gameType: string = "DIGITAL"
+	@Input()
+	gamePrice: string = "R$ 399,99"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
